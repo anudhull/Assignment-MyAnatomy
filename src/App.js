2a1782a72ffaf770b85699b6acab139e6e62e3f9
anudhull <dhullanu99@gmail.com>
2020-05-30 16:29:59 +0530

    Full Stack Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route} from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
+import About from './components/screens/About'
+import Contact from './components/screens/Contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<div className="head">
+      <h1>Home Expenses Management</h1>
+	</div>
+	<div>
+	<BrowserRouter>
+	<NavBar />
+	<Route exact path="/">
+		<Home />
+	</Route>
+	<Route path="/profile">
+		<Profile />
+	</Route>
+	<Route path="/login">
+		<Login />
+	</Route>
+	<Route path="/signup">
+		<Signup />
+	</Route>
+	<Route path="/about">
+		<About />
+	</Route>
+	<Route path="/contact">
+		<Contact />
+	</Route>
+	</BrowserRouter>
+	</div>
     </div>
   );
 }
